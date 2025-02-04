0cf06f26136596a414c2e063fc3795e3ae46b27f
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2025-02-04 16:18:27 -0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,28 +1,44 @@
+"""
+Streaming data consumer
+"""
 from datetime import datetime
 from kafka import KafkaConsumer
 import mysql.connector
-from mysql.connector import Error
 
-TOPIC = 'toll'
+TOPIC='toll'
 DATABASE = 'tolldata'
-USERNAME = '*******'
+USERNAME = '******'
 PASSWORD = '*******'
 
 print("Connecting to the database")
-
-connection = None
-cursor = None
-
 try:
-    connection = mysql.connector.connect(host='localhost',database='tolldata',user='chinmayi',password='chinmayi')
-    
-    if connection.is_connected():
-        print("Connected to database")
-        cursor = connection.cursor()
-    else:
-        print("Failed to connect to the database.")
-except Error as e:
-    print(f"Error connecting to database: {e}")
-    exit(1)  # Exit if unable to connect to the database
+    connection = mysql.connector.connect(host='localhost', database=DATABASE, user=USERNAME, password=PASSWORD)
+except Exception:
+    print("Could not connect to database. Please check credentials")
+else:
+    print("Connected to database")
+cursor = connection.cursor()
 
+print("Connecting to Kafka")
+consumer = KafkaConsumer(TOPIC)
+print("Connected to Kafka")
+print(f"Reading messages from the topic {TOPIC}")
+for msg in consumer:
 
+    # Extract information from kafka
+
+    message = msg.value.decode("utf-8")
+
+    # Transform the date format to suit the database schema
+    (timestamp, vehicle_id, vehicle_type, toll_plaza_id) = message.split(",")
+
+    dateobj = datetime.strptime(timestamp, '%a %b %d %H:%M:%S %Y')
+    timestamp = dateobj.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Loading data into the database table
+
+    sql = "insert into livetolldata values(%s,%s,%s,%s)"
+    result = cursor.execute(sql,(timestamp,vehicle_id,vehicle_type,toll_plaza_id))
+    print(f"A {vehicle_type} was inserted into the database")
+    connection.commit()
+connection.close()
